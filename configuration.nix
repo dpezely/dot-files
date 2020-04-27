8e809c8987774f5d025047ef4e06299edb5b0694
Daniel Pezely <first name at last name dot com>
2020-04-27 16:25:25 -0700

    Dual-boot NixOS 20.03 and Xubuntu 20.04

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# /etc/nixos/configuration.nix                              -*-conf-*-
+# /etc/nixos/configuration.nix                              -*-nix-*-
 
 # 1. This file was bootstrapped via `nixos-generate-config --root /mnt`
 # 2. Copy into /etc/nixos/ which will likely be under /mnt during install
@@ -6,8 +6,19 @@
 # 4. Updates to running system: sudo nixos-rebuild switch
 # 5. Prune occasionally: nix-collect-garbage -d
 
+
+# FIXME: compile a Rust program with: [dependencies] openssl-sys="0.9"
+# https://nixos.wiki/wiki/FAQ/Libraries
+# https://nixos.org/nixpkgs/manual/#chap-multiple-output
+# Hint: `nix-shell -p openssl`
+
+# TODO:
+# - cryptsetup
+# - wired ethernet via USB dongle; might be an issue with kernels newer than 5.3
+
 # See https://nixos.org/nixos/manual/ or configuration.nix(5) man page
-# or when running locally, run ‘nixos-help’.
+# or when running locally, run ‘nixos-help’ which opens the web browser
+# to a `file:` URL.
 
 { config, pkgs, ... }:
 
@@ -50,10 +61,12 @@
   };
 
   # Just in case:
-  # Default kernel in 20.03 is 5.4 stable; 5.5 is EOL stable; 4.19 is prev LTS.
+  # Default kernel in 20.03 is 5.4 stable; 5.6 is stable; 4.19 is prev LTS.
   #boot.kernelPackages = pkgs.linuxPackages_latest;
-  #boot.kernelPackages = nixos.linuxPackages_5_5.kernel;
-  #boot.kernelPackages = nixos.linuxPackages_4_19.kernel;
+  #boot.kernelPackages = pkgs.linuxPackages_5_6;
+  #boot.kernelPackages = pkgs.linuxPackages_4_19;
+
+  boot.cleanTmpDir = true;
 
   networking.hostName = "nixos"; # FIXME: Define your hostname.
 
@@ -65,6 +78,10 @@
   # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.wlp2s0.useDHCP = true;
+  # This Ethernet interface attaches via USB dongle, and when disconnected
+  # will produce log entry, "Timed out waiting for device ..."
+  #FIXME: interface cycles between up/down state with kernel 5.4, 5.5
+  #networking.interfaces.enp0s20u1.useDHCP = true; # "renamed from eth0"
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -85,20 +102,31 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     emacs vim firefox thunderbird wmctrl jumpapp
-    xfce.xfdesktop xfce.xfce4-battery-plugin
+    xfce.xfdesktop xfce.xfce4-battery-plugin xfce.xfce4-screenshooter
+    xfce.xfce4-pulseaudio-plugin xfce.xfce4-volumed-pulse
     xfce.ristretto xorg.oclock
+    #networkmanager networkmanagerapplet
+    plank #xfce.xfce4-panel
     dict dictdDBs.wordnet wordnet xfce.xfce4-dict dictdDBs.wiktionary
     git ripgrep ripgrep-all xsv
-    gnumake pandoc
-    rPackages.rust rls rustracer rustfmt rustup cargo cargo-outdated clippy diesel-cli
-    sbcl rPackages.hyperSpec lispPackages.swank lispPackages.quicklisp
+    clang gnumake pandoc binutils pkg-config libressl zlib
+
+    # Packages for Rust and Common-Lisp work fine for convention use:
+    # rPackages.rust rls rustracer rustfmt rustup cargo cargo-outdated clippy diesel-cli
+    # sbcl rPackages.hyperSpec lispPackages.swank lispPackages.quicklisp
+    # As critical path tools, however, those will be installed
+    # directly from official upstream distributions channels.
+    # HOWEVER, official `rustup` won't install as-is, so need it from pkgs:
+    rustup
+
     python3 #python3Full
     wget whois traceroute
     mpd mpc_cli python38Packages.notify2 #xfce.xfce4-mpc-plugin
     # audacity blender inkscape imagemagick
     docker vagrant virtualbox
+
     # These require .allowUnfree=true:
-    teams google-chrome
+    google-chrome teams freeoffice microcodeIntel #microcodeAmd
   ];
   nixpkgs.config.allowUnfree = true;
 
@@ -119,8 +147,6 @@
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 22 ];
   # networking.firewall.allowedTCPPortRanges = [ { from = 8080; to = 8089 } ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = true;
   networking.firewall.allowPing = false;
 
@@ -148,11 +174,29 @@
     # xkbOptions = "eurosign:e";
   };
 
+  environment.variables = {
+    # Play nice with dual-boot Linux (e.g., Xubuntu) where PATHs
+    # differ (such as for autostart, plank, jumpapp), because NixOS
+    # doesn't abide by Linux FHS policies.  Maybe begin with:
+    # `rsync -a ~/.config/ ~/.config_nixos/`
+    # However, avoid hard-links or sym-links, in case any particular
+    # app modifies a config file in-place.
+    #
+    # Then, convert keyboard shortcut settings to use NixOS paths:
+    # sed 's%/usr/bin/%/run/current-system/sw/bin/%g' \
+    #  < ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+    #  > ~/.config_nixos/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+    #
+    # Perform similar changes for each Autostart and Plank Launcher files:
+    # ~/.config/autostart/*.desktop, ~/.config/plank/dock*/launchers/*.dockitem
+    XDG_CONFIG_HOME = "$HOME/.config_nixos";
+  };
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.daniel = {
     description = "Daniel";
     isNormalUser = true;
-    extraGroups = [ "wheel" "staff" "docker" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "staff" "docker" ];
   };
 
   # Before changing this value read the documentation for this option
